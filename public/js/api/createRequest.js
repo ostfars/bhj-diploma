f0eb9b1487d388b2e782b12986ec47b6ec66ba95
ostfars <v_minin@rambler.ru>
2022-09-02 22:49:09 +0300

    initial commit

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,19 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest();
 
+  let url = options.url;
+  const formData = new FormData();
+
+  if (options.data) {
+    if (options.method === "GET") {
+      url += '?' + Object.entries(options.data).map(
+        ([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+      )
+    }
+  }
+
+  xhr.open(options.method, url);
+  xhr.send();
 };
