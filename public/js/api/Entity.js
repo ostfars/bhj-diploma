73d550fcac2d4c65f99ee64e087de2f4790a7952
Vladimir Minin <v_minin@rambler.ru>
2022-09-03 23:47:40 +0300

    entity

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -9,7 +9,12 @@ class Entity {
    * (в зависимости от того, что наследуется от Entity)
    * */
   static list(data, callback){
-
+    createRequest({
+      url: this.URL,
+      method: 'GET',
+      data,
+      callback
+    });
   }
 
   /**
@@ -18,7 +23,12 @@ class Entity {
    * что наследуется от Entity)
    * */
   static create(data, callback) {
-
+    createRequest({
+      url: this.URL,
+      method: 'PUT',
+      data,
+      callback
+    });
   }
 
   /**
@@ -26,6 +36,13 @@ class Entity {
    * (в зависимости от того, что наследуется от Entity)
    * */
   static remove(data, callback ) {
-
-  }
+    createRequest({
+      url: this.URL,
+      method: 'DELETE',
+      data,
+      callback
+    });
+  }  
 }
+
+Entity.URL = '';
